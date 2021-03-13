0d2caa5fb16badbb58df1bed4383ddad2b6511b6
James Masaviru <jmasaviru@gmail.com>
2021-03-13 18:00:25 -0500

    Updating user message

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ function Navbar(props){
         <div id="nav" className="card text-center">
         <div className="card-header">
           <div>Employee Directory</div>
-          <h4>Click on the Name title headline to view list in ascending/descending alphabetical order or use the search box to narrow your result</h4>
+          <h4>Click on 'Name' headline to view list of employees in ascending or descending alphabetical order or use the search box to narrow your search</h4>
         </div>
     </div>
     <div id="green"></div><br></br>
