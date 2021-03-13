a2e14f7a0805e5b506cfefb0848715d940a6dced
James Masaviru <jmasaviru@gmail.com>
2021-03-13 17:54:36 -0500

    Updating background & search

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar(props){
+    return(
+      <div>
+        <div id="nav" className="card text-center">
+        <div className="card-header">
+          <div>Employee Directory</div>
+          <h4>Click on the Name title headline to view list in ascending/descending alphabetical order or use the search box to narrow your result</h4>
+        </div>
+    </div>
+    <div id="green"></div><br></br>
+    </div>
+    );
+    
+}
+
+export default Navbar;
